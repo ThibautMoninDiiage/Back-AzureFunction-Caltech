6e3b1d103e585542d5a35bbaeaf9a177f9e674a0
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-11-23 14:04:55 +0100

    Test Sécurité

--- AzureFunctionEFCore/AzureFunctionEFCore/StartUp.cs
+++ AzureFunctionEFCore/AzureFunctionEFCore/StartUp.cs
@@ -7,6 +7,7 @@ using AzureFunction.Contract.UnitOfWork;
 using AzureFunction.UnitOfWork.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 using AzureFunction.UnitOfWork.DbContextAZ;
+using Microsoft.IdentityModel.Protocols;
 
 [assembly: FunctionsStartup(typeof(AzureFunctionEFCore.StartUp))]
 
@@ -16,7 +17,7 @@ namespace AzureFunctionEFCore
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            string connString = "Server=tcp:test-server-database.database.windows.net,1433;Initial Catalog=testdbdiiagedeux;Persist Security Info=False;User ID=CloudSAf42aaec6;Password=Azerty@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string connString = Environment.GetEnvironmentVariable("SqlConnectionString", EnvironmentVariableTarget.Process);
             builder.Services.AddDbContext<DbContextServeur>(options => options.UseSqlServer(connString));
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddTransient(typeof(IRoleService), typeof(RoleService));
