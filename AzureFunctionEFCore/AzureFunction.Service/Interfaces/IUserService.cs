b8c603ddac0277c15569c29cf6766dde7428f95c
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-11-24 10:46:26 +0100

    Ajouts des routes, services et uow pour la connexion et le GetById

--- AzureFunctionEFCore/AzureFunction.Service/Interfaces/IUserService.cs
+++ AzureFunctionEFCore/AzureFunction.Service/Interfaces/IUserService.cs
@@ -1,9 +1,12 @@
 ﻿using AzureFunction.Models;
+using AzureFunction.Models.DTO.Down;
+using AzureFunction.Models.DTO.Up;
 
 namespace AzureFunction.Service.Interfaces
 {
     public interface IUserService
     {
         public Task<User> Get(string login,string password);
+        public Task<UserDtoDown> GetById(int id);
     }
 }
--- AzureFunctionEFCore/AzureFunctionEFCore/Users.cs
+++ AzureFunctionEFCore/AzureFunctionEFCore/Users.cs
@@ -5,9 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using AzureFunction.Service.Interfaces;
+using Microsoft.Extensions.Logging;
+using AzureFunction.Models.DTO.Up;
 
 namespace AzureFunctionEFCore
 {
@@ -21,23 +22,57 @@ namespace AzureFunctionEFCore
             _userService = userService;
         }
 
-        [FunctionName("Users")]
-        public async Task<IActionResult> Get([HttpTrigger(AuthorizationLevel.Anonymous,"get", Route = Route)] HttpRequest req,ILogger log)
+        [FunctionName("Connexion")]
+        public async Task<IActionResult> Connexion([HttpTrigger(AuthorizationLevel.Anonymous,"post", Route = Route)] HttpRequest req)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            UserDtoUp userDtoUp = JsonConvert.DeserializeObject<UserDtoUp>(requestBody);
 
-            string login = req.Query["login"];
-            string mdp = req.Query["password"];
-
-            if(login == null || mdp == null)
+            if (userDtoUp.UserName == null || userDtoUp.Password == null)
             {
                 return new BadRequestResult();
             }
 
-            var result = await _userService.Get(login,mdp);
+            var result = await _userService.Get(userDtoUp.UserName, userDtoUp.Password);
 
+            if (result == null)
+            {
+                return new EmptyResult();
+            }
 
             return new OkObjectResult(result);
+        }
+
+        [FunctionName("UserById")]
+        public async Task<IActionResult> GetById([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Route)] HttpRequest req,ILogger log)
+        {
+            try
+            {
+                string idRecu = req.Query["id"];
+                if (idRecu == null)
+                {
+                    return new BadRequestResult();
+                }
+                int id = int.Parse(idRecu);
+                if (id == 0)
+                {
+                    return new BadRequestResult();
+                }
+
+                var result = await _userService.GetById(id);
+
+                if (result == null)
+                {
+                    return new EmptyResult();
+                }
+
+                return new OkObjectResult(result);
+            }
+            catch(AggregateException ex)
+            {
+                log.LogInformation(ex.Message);
+                return new BadRequestResult();
+            }
         }
     }
 }
