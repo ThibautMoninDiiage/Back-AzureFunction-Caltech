de2e3ff10bff097bb110117db77581bcb2bac4e4
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-12-14 10:04:56 +0100

    Test sans swagger

--- AzureFunctionEFCore/SecurityServer.Function/Roles.cs
+++ AzureFunctionEFCore/SecurityServer.Function/Roles.cs
@@ -22,7 +22,6 @@ namespace SecurityServer.Function
         }
 
         [FunctionName("GetAllRoles")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "Role" })]
         public async Task<IActionResult> GetAllRoles([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Route)]HttpRequest req,ILogger logger)
         {
             IEnumerable<RoleDtoDown> result = await _roleService.GetAll();
