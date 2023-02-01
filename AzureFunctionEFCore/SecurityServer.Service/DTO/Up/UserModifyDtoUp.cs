62a6fc88125bf95f294a83aebc4c3ba9f6aaa8c7
Tristan DEVOILLE <tristan.devoille@diiage.org>
2023-02-01 16:28:16 +0100

    Correction

--- AzureFunctionEFCore/SecurityServer.Service/DTO/Up/UserModifyDtoUp.cs
+++ AzureFunctionEFCore/SecurityServer.Service/DTO/Up/UserModifyDtoUp.cs
@@ -6,7 +6,6 @@
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Username { get; set; }
-        public string? Password { get; set; }
         public string? Avatar { get; set; }
         public string? Mail { get; set; }
     }
